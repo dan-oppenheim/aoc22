df02ab31a9d40d2778baed850b6995dc5b50aa3a
Dan Oppenheim <dan@oppenheim.me.uk>
2022-11-30 22:40:12 +0000

    Update fetch_input util to fetch from 2022 AOC
    Also actually write the data to file

--- utils/fetch_input.py
+++ utils/fetch_input.py
@@ -1,6 +1,6 @@
 import requests
 
-with open("session.txt") as session_file:
+with open("utils/session.txt") as session_file:
     session_cookie = session_file.readline()
 
 day = input("Which day? ")
@@ -9,12 +9,15 @@ try:
     if day < 0 or day > 24:
         raise ValueError()
 
-    request = requests.get(f"https://adventofcode.com/2021/day/{day}/input", 
+    request = requests.get(f"https://adventofcode.com/2022/day/{day}/input", 
         headers={
             'cookie':f"session={session_cookie}"
         }
     )
-    print(request.text)
+
+    with open(f"input/day{day}.txt", 'w') as out_file:
+        print(request.text, file=out_file)
+
 except Exception as e:
     print(e)
     print("Please enter a number between 1 and 24")
