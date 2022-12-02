e8962641fba6d01023d88304503a3274074ef84e
Dan Oppenheim <dan@oppenheim.me.uk>
2022-12-02 10:08:37 +0000

    Implemented day 2

---
+++ day02.py
@@ -0,0 +1,33 @@
+combos = {
+    "A X": 3 + 1,
+    "A Y": 6 + 2,
+    "A Z": 0 + 3,
+    "B X": 0 + 1,
+    "B Y": 3 + 2,
+    "B Z": 6 + 3,
+    "C X": 6 + 1,
+    "C Y": 0 + 2,
+    "C Z": 3 + 3,
+    "": 0
+}
+
+
+with open("input/day2.txt") as input_file:
+    print(sum(combos[line.strip()] for line in input_file))
+
+
+decrypted_combos = {
+    "A X": 0 + 3,
+    "A Y": 3 + 1,
+    "A Z": 6 + 2,
+    "B X": 0 + 1,
+    "B Y": 3 + 2,
+    "B Z": 6 + 3,
+    "C X": 0 + 2,
+    "C Y": 3 + 3,
+    "C Z": 6 + 1,
+    "": 0
+}
+
+with open("input/day2.txt") as input_file:
+    print(sum(decrypted_combos[line.strip()] for line in input_file))
